fbdb34c4c609a683024c08dcb765ceecb5595a76
CarterBartram-Tufts <carter.bartram@tufts.edu>
2024-11-18 17:35:08 -0500

    Update populate_tables_test.sql

--- populate_tables_test.sql
+++ populate_tables_test.sql
@@ -1,11 +1,11 @@
 INSERT INTO products(productid, name, price)
-  VALUES (0, 'Test Product', 9.99); 
+  VALUES (0, 'Test Product', 9.99, 'test'); 
 
 INSERT INTO customers(customerid, first_name, last_name, email, address_1, address_2, city, state, zip_code)
   VALUES (0, 'Test', 'Customer', 'carter.bartram@tufts.edu', '4019 Boston Ave', NULL, 'Medford', 'MA', 02155);
 
 INSERT INTO orders(orderid, customerid, productid, artworkid, quantity, total_price, order_date)
-  VALUES (0, 0, 0, '1979.486.1', 1, 9.99, '2024-11-17 16:26:32.000000');
+  VALUES (0, 0, 0, '1979.486.1', 1, 9.99, '2024-11-17 16:26:32.000000', 'submitted');
 
 SET FOREIGN_KEY_CHECKS = 0;
 TRUNCATE orders; 
