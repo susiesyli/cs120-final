e2af4daefcf58d25dfd7b0b6b6bf4832d7648ed5
CarterBartram-Tufts <carter.bartram@tufts.edu>
2024-11-24 19:12:05 -0500

    Create list_products.php
    Example PHP to list products from database. Can be local or remote depending on where we end up hosting.

---
+++ list_products.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>  
+<html>
+    <head>
+        <?php
+            $server = "localhost";
+            $userid = "u3fi6z83zcg2y";
+            $pw = "artify1234";
+            $db = "dbgylh79jpnkih";
+            $conn = new mysqli($server, $userid, $pw);
+            $conn->select_db($db);
+        ?>
+    </head>
+    <body>
+        <div class="product-options">
+        <?php
+            $result = $conn->query("SELECT * FROM products;");
+
+            while ($row = $result->fetch_assoc()) {
+                printf(
+                    '<div class="product-options"><img class="product-image" src="%s"><div><h3>%s</h3><p>%s</p><p class="price">From $%s</p></div>',
+                    $row['image_link'],
+                    $row['name'],
+                    $row['description'],
+                    $row['price']
+                );
+            }
+            $conn->close();
+        ?>
+        </div>
+    </body>
+</html>
